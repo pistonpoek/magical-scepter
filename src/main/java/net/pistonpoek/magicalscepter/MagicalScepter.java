db346174213b74392977c2704178d5ccce616923
pistonpoek <pistonpoek@gmail.com>
2024-09-15 19:20:55 +0200

    Utilize ServerLivingEntityEvents

--- src/main/java/net/pistonpoek/magicalscepter/MagicalScepter.java
+++ src/main/java/net/pistonpoek/magicalscepter/MagicalScepter.java
@@ -2,12 +2,15 @@ package net.pistonpoek.magicalscepter;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.pistonpoek.magicalscepter.component.ModDataComponentTypes;
 import net.pistonpoek.magicalscepter.entity.effect.ModStatusEffects;
 import net.pistonpoek.magicalscepter.item.ModItems;
 import net.pistonpoek.magicalscepter.recipe.ModRecipeSerializer;
 import net.pistonpoek.magicalscepter.registry.ModIdentifier;
 import net.pistonpoek.magicalscepter.registry.ModRegistries;
+import net.pistonpoek.magicalscepter.scepter.ScepterInfusion;
+import net.pistonpoek.magicalscepter.spell.Spell;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,5 +24,7 @@ public class MagicalScepter implements ModInitializer {
 		ModRecipeSerializer.init();
 		ModStatusEffects.init();
 		ModDataComponentTypes.init();
+		ServerLivingEntityEvents.AFTER_DEATH.register(Spell.Cast::afterDeath);
+		ServerLivingEntityEvents.AFTER_DAMAGE.register(ScepterInfusion::afterDamage);
 	}
 }
