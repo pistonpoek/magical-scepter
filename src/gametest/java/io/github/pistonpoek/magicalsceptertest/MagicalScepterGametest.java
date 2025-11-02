4a7456a619e4641e72dc504d05b5c3ef65ebb6a9
pistonpoek <pistonpoek@gmail.com>
2025-11-02 14:05:11 +0100

    Add Game Test to build
    Add the game test directory for both server and client game tests.

---
+++ src/gametest/java/io/github/pistonpoek/magicalsceptertest/MagicalScepterGametest.java
@@ -0,0 +1,13 @@
+package io.github.pistonpoek.magicalsceptertest;
+
+import net.fabricmc.fabric.api.gametest.v1.CustomTestMethodInvoker;
+import net.minecraft.test.TestContext;
+
+import java.lang.reflect.Method;
+
+public class MagicalScepterGametest implements CustomTestMethodInvoker {
+    @Override
+    public void invokeTestMethod(TestContext context, Method method) throws ReflectiveOperationException {
+
+    }
+}
