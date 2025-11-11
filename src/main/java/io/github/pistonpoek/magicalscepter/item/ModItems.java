1ac0721e547eb83d52454798e216f4efb88a3926
pistonpoek <pistonpoek@gmail.com>
2025-11-11 14:14:23 +0100

    Update ModItems.java
    Make scepter repairable using lapis lazuli

--- src/main/java/io/github/pistonpoek/magicalscepter/item/ModItems.java
+++ src/main/java/io/github/pistonpoek/magicalscepter/item/ModItems.java
@@ -31,10 +31,10 @@ import java.util.function.Function;
 public class ModItems {
     public static final Item SCEPTER = register("scepter", new Item.Settings().maxCount(1).rarity(Rarity.RARE));
     public static final Item ARCANE_SCEPTER = register("arcane_scepter", ArcaneScepterItem::new,
-            new Item.Settings().maxDamage(64).rarity(Rarity.RARE)
+            new Item.Settings().maxDamage(64).repairable(Items.LAPIS_LAZULI).rarity(Rarity.RARE)
                     .component(ModDataComponentTypes.SCEPTER_EXPERIENCE, ScepterExperienceComponent.DEFAULT));
     public static final Item MAGICAL_SCEPTER = register("magical_scepter", MagicalScepterItem::new,
-            new Item.Settings().maxDamage(64).rarity(Rarity.RARE)
+            new Item.Settings().maxDamage(64).repairable(Items.LAPIS_LAZULI).rarity(Rarity.RARE)
                     .component(ModDataComponentTypes.SCEPTER_CONTENTS, ScepterContentsComponent.DEFAULT));
 
     public static final Item SORCERER_SPAWN_EGG = register("sorcerer_spawn_egg", SpawnEggItem::new,
