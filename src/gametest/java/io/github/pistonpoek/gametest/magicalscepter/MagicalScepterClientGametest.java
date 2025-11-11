e0bf63933bfe8cc1a16b2c55be889cf144f929c8
pistonpoek <pistonpoek@gmail.com>
2025-11-11 17:04:06 +0100

    Update fabric.mod.json
    Use gradle properties in fabric mod json for version numbers.
    Update license display to be more clear.
    Reduce fabric.mod.json for game test to be static.

--- src/gametest/java/io/github/pistonpoek/gametest/magicalscepter/MagicalScepterClientGametest.java
+++ src/gametest/java/io/github/pistonpoek/gametest/magicalscepter/MagicalScepterClientGametest.java
@@ -1,11 +1,11 @@
-package io.github.pistonpoek.magicalsceptertest;
+package io.github.pistonpoek.gametest.magicalscepter;
 
 import net.fabricmc.fabric.api.gametest.v1.CustomTestMethodInvoker;
 import net.minecraft.test.TestContext;
 
 import java.lang.reflect.Method;
 
-public class MagicalScepterGametest implements CustomTestMethodInvoker {
+public class MagicalScepterClientGametest implements CustomTestMethodInvoker {
     @Override
     public void invokeTestMethod(TestContext context, Method method) throws ReflectiveOperationException {
 
