f404273d39bb34dd5765f3420f359bc6edc55062
pistonpoek <pistonpoek@gmail.com>
2025-11-12 16:59:16 +0100

    Update MagicalScepterClientGametest.java
    Fix client game test class structure by adding FabricClientGameTest interface implementation.

--- src/gametest/java/io/github/pistonpoek/gametest/magicalscepter/MagicalScepterClientGametest.java
+++ src/gametest/java/io/github/pistonpoek/gametest/magicalscepter/MagicalScepterClientGametest.java
@@ -1,13 +1,19 @@
 package io.github.pistonpoek.gametest.magicalscepter;
 
+import net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest;
+import net.fabricmc.fabric.api.client.gametest.v1.context.ClientGameTestContext;
 import net.fabricmc.fabric.api.gametest.v1.CustomTestMethodInvoker;
 import net.minecraft.test.TestContext;
 
 import java.lang.reflect.Method;
 
-public class MagicalScepterClientGametest implements CustomTestMethodInvoker {
+public class MagicalScepterClientGameTest implements CustomTestMethodInvoker, FabricClientGameTest {
     @Override
     public void invokeTestMethod(TestContext context, Method method) throws ReflectiveOperationException {
 
+    }
+
+    @Override
+    public void runTest(ClientGameTestContext context) {
     }
 }
