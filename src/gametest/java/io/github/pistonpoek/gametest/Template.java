9a97121d84f6f41b1a748df403af5231b5984b30
pistonpoek <pistonpoek@gmail.com>
2025-11-12 17:26:54 +0100

    Create Template.java
    Add template class for game tests

---
+++ src/gametest/java/io/github/pistonpoek/gametest/Template.java
@@ -0,0 +1,20 @@
+package io.github.pistonpoek.gametest;
+
+import net.fabricmc.fabric.api.gametest.v1.CustomTestMethodInvoker;
+import net.fabricmc.fabric.api.gametest.v1.GameTest;
+import net.minecraft.test.TestContext;
+
+import java.lang.reflect.Method;
+
+public class Template implements CustomTestMethodInvoker {
+    @Override
+    public void invokeTestMethod(TestContext context, Method method) throws ReflectiveOperationException {
+        method.invoke(this, context);
+    }
+
+    @GameTest(structure="minecraft:empty")
+    public void empty(TestContext context) {
+        new TestBlockChecker(context).start();
+        context.complete();
+    }
+}
