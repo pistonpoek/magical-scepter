51e6717008a40e7fda3c6c77bd99c6788dd78a73
pistonpoek <pistonpoek@gmail.com>
2025-11-12 17:31:41 +0100

    Add Client Template
    Add template for client game tests.

---
+++ src/gametest/java/io/github/pistonpoek/gametest/ClientTemplate.java
@@ -0,0 +1,11 @@
+package io.github.pistonpoek.gametest;
+
+import net.fabricmc.fabric.api.client.gametest.v1.FabricClientGameTest;
+import net.fabricmc.fabric.api.client.gametest.v1.context.ClientGameTestContext;
+
+public class ClientTemplate implements FabricClientGameTest {
+    @Override
+    public void runTest(ClientGameTestContext context) {
+
+    }
+}
